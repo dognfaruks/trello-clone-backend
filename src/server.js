8f993ed2cffbac7f6d13224bea7b0a4cfd2e69c0
ömer faruk doğan <farukdogann192@gmail.com>
2026-08-22 01:08:46 +0300

    fix trust proxy setting for rate limit on Render

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
 const app = express();
+app.set('trust proxy', 1);
 
 app.use(cors());
 app.use(express.json());
